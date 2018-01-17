a4780acbb80d56ec742d612102403738f297f7fa
nprabhu <nprabhu@vera.com>
2018-01-17 11:30:22 +0530

    Revert "Bug fix for Android 5.1"
    This reverts commit acfdf641b8196eb47c39c43fc097a2ca1fee77fd.

--- app/src/main/java/naren/income/expense/ui/MainActivity.java
+++ app/src/main/java/naren/income/expense/ui/MainActivity.java
@@ -63,6 +63,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         smsComponentName = new ComponentName(getBaseContext().getPackageName(), SmsReceiver.class.getName());
 
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         findViewById(R.id.fab).setOnClickListener(this);
 
         mListView = findViewById(R.id.main_list);
